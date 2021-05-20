a72b59f8449c5740a6724b097abd05eefad980ec
jbrasko <jbrasko0508@gmail.com>
2021-05-20 15:29:13 -0700

    move npm installs to correct location, set up bird card test

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import BirdCard from './components/BirdCard'
 
 function App() {
   return (
     <div>
-      Hello
+      <BirdCard />
     </div>
   );
 }
--- client/src/components/BirdCard.js
+++ client/src/components/BirdCard.js
@@ -4,6 +4,11 @@ import styled from 'styled-components/macro'
 const BirdCard = props => {
     const { name, sciName, order, family, genus, species, conserv, desc, imgUrl } = props
 
+    return (
+        <Test>Hello Test</Test>
+    )
 }
+
+const Test = styled.div``
 
 export default BirdCard
