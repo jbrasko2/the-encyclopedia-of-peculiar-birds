9d6b134623244adb310b84ae4e5c7119e14bb8c4
jbrasko <jbrasko0508@gmail.com>
2021-05-20 15:19:04 -0700

    set up bird card component

---
+++ client/src/components/BirdCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+
+const BirdCard = props => {
+    const { name, sciName, order, family, genus, species, conserv, desc, imgUrl } = props
+
+}
+
+export default BirdCard
