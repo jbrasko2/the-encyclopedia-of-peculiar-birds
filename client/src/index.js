f9446608fe67ad02c929ad2adf1f0a4e355d68dd
jbrasko <jbrasko0508@gmail.com>
2021-05-20 15:58:04 -0700

    update index to take redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,18 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, compose, applyMiddleware } from 'redux'
+import { reducer } from './redux/reducer'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import { HashRouter } from 'react-router-dom'
+
+const store = createStore(reducer, compose(
+  applyMiddleware(thunk),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
