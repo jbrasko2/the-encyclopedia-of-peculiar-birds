ec3eaa69dde27a0fbaacd5b8ffb2482f810303b4
jbrasko <jbrasko0508@gmail.com>
2021-05-25 11:42:21 -0700

    sort list page

--- client/src/containers/ListPage.js
+++ client/src/containers/ListPage.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components/macro'
+import { Link } from 'react-router-dom'
 
 class ListPage extends Component {
     render() {
+        const sortedBirds = this.props.birds.sort((a, b) => (a.name > b.name) ? 1 : -1)
+
         return (
             <ul>
-                {this.props.birds.map(bird => {
-                     return <li key={bird.id}>{bird.name} ({bird.scientificName})</li>
+                {sortedBirds.map(bird => {
+                     return (
+                         <Link to={"/birds/" + bird.id}>
+                            <li key={bird.id}>{bird.name} ({bird.scientificName})</li>
+                         </Link>
+                     )
                 })}
             </ul>
         )
