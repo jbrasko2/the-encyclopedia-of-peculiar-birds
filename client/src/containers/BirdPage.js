f9a02dfd7733761bdcb63089d9aeb0e0f698d166
jbrasko <jbrasko0508@gmail.com>
2021-05-25 11:12:45 -0700

    update scientific classification styling

--- client/src/containers/BirdPage.js
+++ client/src/containers/BirdPage.js
@@ -21,10 +21,10 @@ class BirdPage extends Component {
                 <Name>{name}</Name>
                 <Scientific>{scientificName}</Scientific>
                 <BirdImage src={require('../bird-images/' + imgUrl).default} />
-                <Classification>{order}</Classification>
-                <Classification>{family}</Classification>
-                <SpecificClass>{genus}</SpecificClass>
-                <SpecificClass>{species}</SpecificClass>
+                <p>Order: {order}</p>
+                <p>Family: {family}</p>
+                <p>Genus: {genus}</p>
+                <p>Species: {species}</p>
                 <Description>{description}</Description>
             </Wrapper>
         )
@@ -43,6 +43,10 @@ const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    p {
+        margin: 0;
+    }
 `
 
 const Name = styled.div`
@@ -59,19 +63,13 @@ const BirdImage = styled.img`
     height: 600px;
     width: 450px;
     margin: 24px;
-`
-
-const Classification = styled.div`
-
-`
-
-const SpecificClass = styled.div`
-    font-style: italic;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px;
 `
 
 const Description = styled.div`
     max-width: 800px;
     font-size: 1.25rem;
+    margin: 12px;
 `
 
 const mapStateToProps = state => ({
