2d05eaa141041e4181249f133c0a0cc037a85671
jbrasko <jbrasko0508@gmail.com>
2021-05-28 12:56:36 -0700

    add delay to footer

--- client/src/containers/Footer.js
+++ client/src/containers/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import styled from 'styled-components/macro'
+import { FadeIn } from 'animate-css-styled-components'
 
 const Footer = () => {
     return (
+        <FadeIn delay="2s">
         <Wrapper>
             <ImageCredit>
                 Images Courtesy of the John James Audubon Center at Mill Grove, Montgomery County Audubon Collection, and Zebra Publishing
             </ImageCredit>
         </Wrapper>
+        </FadeIn>
     )
 }
 
