3b9269408724c6fc083a9c16b639970602865fe1
jbrasko <jbrasko0508@gmail.com>
2021-06-30 13:51:23 -0500

    fix formatting

--- client/src/containers/BirdCardContainer.js
+++ client/src/containers/BirdCardContainer.js
@@ -1,40 +1,40 @@
-import React from 'react'
-import BirdCard from '../components/BirdCard'
-import SearchBar from '../components/SearchBar'
-import styled from 'styled-components/macro'
-import { FadeIn } from 'animate-css-styled-components'
-import Footer from '../components/Footer'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import BirdCard from '../components/BirdCard';
+import SearchBar from '../components/SearchBar';
+import styled from 'styled-components/macro';
+import { FadeIn } from 'animate-css-styled-components';
+import Footer from '../components/Footer';
+import { useSelector } from 'react-redux';
 
 const BirdCardContainer = () => {
-    const birds = useSelector(state => state.birds.birds)
-    const requesting = useSelector(state => state.birds.requesting)
-    
-    const renderPage = () => {
-        return (
-            <>
-                <FadeIn delay=".25s" duration="2s">
-                <SearchBar />
-                <Wrapper>
-                    {birds.map(bird => <BirdCard key={bird.id} {...bird} />)}
-                </Wrapper>
-                </FadeIn>
-                <Footer />
-            </>
-        )
-    }
+  const birds = useSelector(state => state.birds.birds);
+  const requesting = useSelector(state => state.birds.requesting);
 
-    const renderSpinner = () => <div className="loader">Spinner</div>
-    
+  const renderPage = () => {
     return (
-        requesting ? renderSpinner() : renderPage()
-    )
-}
+      <>
+        <FadeIn delay='.25s' duration='2s'>
+          <SearchBar />
+          <Wrapper>
+            {birds.map(bird => (
+              <BirdCard key={bird.id} {...bird} />
+            ))}
+          </Wrapper>
+        </FadeIn>
+        <Footer />
+      </>
+    );
+  };
+
+  const renderSpinner = () => <div className='loader'>Spinner</div>;
+
+  return requesting ? renderSpinner() : renderPage();
+};
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
 
-export default BirdCardContainer
+export default BirdCardContainer;
